da99db15b209b16f4ce1cad15d906f67d4592585
Tymek144 <tym.pom.2g@gmail.com>
2026-06-11 17:55:35 +0200

    drugi zbior danych

---
+++ R/dane.R
@@ -0,0 +1,21 @@
+#' Przykładowe przepisy kulinarne w jednostkach
+#'
+#' Zbiór danych zawiera przykładowe przepisy kulinarne wraz z listą składników,
+#' liczbą porcji, ilością składnika oraz jednostką miary.
+#'
+#' Dane służą do pokazania działania funkcji skaluj_przepis() dostępnej w pakiecie
+#' recipleScaleR.
+#'
+#' @format Ramka danych zawierająca 8 obserwacji i 5 zmiennych:
+#' \describe{
+#'   \item{przepis}{Nazwa przepisu.}
+#'   \item{porcje}{Liczba porcji w przepisie bazowym.}
+#'   \item{skladnik}{Nazwa składnika.}
+#'   \item{ilosc}{Ilość danego składnika.}
+#'   \item{jednostka}{Jednostka miary składnika.}
+#' }
+#'
+#' @examples
+#' przepisy_przykladowe
+#'
+"przepisy_przykladowe"
---
+++ data-raw/przepisy_w_miarach_kuchennych.R
@@ -0,0 +1,26 @@
+przepisy_przykladowe <- data.frame(
+  przepis = c(
+    "ciasto_kakaowe", "ciasto_kakaowe", "ciasto_kakaowe", "ciasto_kakaowe", "ciasto_kakaowe",
+    "owsianka_kakaowa", "owsianka_kakaowa", "owsianka_kakaowa", "owsianka_kakaowa", "owsianka_kakaowa"
+  ),
+  porcje = c(
+    8, 8, 8, 8, 8,
+    2, 2, 2, 2, 2
+  ),
+  skladnik = c(
+    "maka", "cukier", "mleko", "olej", "kakao",
+    "platki_owsiane", "mleko", "kakao", "cukier", "maslo"
+  ),
+  ilosc = c(
+    2, 1, 1, 0.5, 3,
+    1, 2, 1, 2, 1
+  ),
+  jednostka = c(
+    "szklanka", "szklanka", "szklanka", "szklanka", "lyzka",
+    "szklanka", "szklanka", "lyzka", "lyzeczka", "lyzeczka"
+  ),
+  stringsAsFactors = FALSE
+)
+
+# Zapisujemy dane do pakietu
+usethis::use_data(przepisy_przykladowe, overwrite = TRUE)
